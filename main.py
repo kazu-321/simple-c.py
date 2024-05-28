00eb828389d850c856a5c287734621804e440c92
kazu-321 <kzs321kzs@gmail.com>
2024-05-29 03:21:39 +0900

    refactor: Improve compiler class initialization and variable handling

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 CODE=[
 "int a;",
-"int b=10;",
-"a=a+10;"
+"int b;",
+"b=10;",
+"a=b+10;"
 ]
 # I have to convert this program to scratch.mit.edu.
 # So, I must use easy(decreased in scratch) code.
@@ -23,14 +24,14 @@ class compiler:
         if V_NAMES not in self.V_NAMES:
             self.V_TYPES.append(V_TYPES)
             self.V_NAMES.append(V_NAMES)
-            self.V_VALUE.append()
+            self.V_VALUE.append(None)
     
     def assignment(self,V_NAMES,v_value):
         if V_NAMES in self.V_NAMES:
-            self.V_VALUE[self.V_NAMES.find(V_NAMES)]=v_value
+            self.V_VALUE[self.V_NAMES.index(V_NAMES)]=v_value
     
     def remove_variable(self,V_NAMES):
-        rm=self.V_NAMES.find(V_NAMES)
+        rm=self.V_NAMES.index(V_NAMES)
         if len(self.V_NAMES)>rm:
             self.V_TYPES[rm]=self.V_TYPES[len(self.V_TYPES)]
             self.V_NAMES[rm]=self.V_NAMES[len(self.V_NAMES)]
@@ -40,43 +41,43 @@ class compiler:
         self.V_VALUE[len(self.V_VALUE)].pop(-1)
 
     def increase(self,V_NAMES):
-        self.assignment(V_NAMES,self.V_VALUE[self.V_NAMES.find(V_NAMES)]+1)
+        self.assignment(V_NAMES,self.V_VALUE[self.V_NAMES.index(V_NAMES)]+1)
 
     def substr(self,value,start,end,return_variable):
         self.initialize_variable("int","__substr.counter")
         self.assignment("__substr.counter",start)
         self.assignment(return_variable,"")
         for _ in range(end-start+1):
-            self.assignment(self.V_VALUE[self.V_NAMES.find(return_variable)],self.V_VALUE[self.V_NAMES.find(return_variable)]+value[self.V_VALUE[self.V_NAMES.find("__substr.counter")]])
+            self.assignment(self.V_VALUE[self.V_NAMES.index(return_variable)],self.V_VALUE[self.V_NAMES.index(return_variable)]+value[self.V_VALUE[self.V_NAMES.index("__substr.counter")]])
             self.increase("__substr.counter")
         self.remove_variable("__substr.counter")
 
-    def findchar(self,value,char,return_variable):
-        self.initialize_variable("int","__findchar.counter")
-        self.assignment("__findchar.counter",0)
+    def indexchar(self,value,char,return_variable):
+        self.initialize_variable("int","__indexchar.counter")
+        self.assignment("__indexchar.counter",0)
         self.assignment(return_variable,-1)
         for _ in value:
-            if value[self.V_VALUE[self.V_NAMES.find("__findchar.counter")]]==char:
-                self.assignment(return_variable,self.V_VALUE[self.V_NAMES.find("__findchar.counter")])
+            if value[self.V_VALUE[self.V_NAMES.index("__indexchar.counter")]]==char:
+                self.assignment(return_variable,self.V_VALUE[self.V_NAMES.index("__indexchar.counter")])
                 return
-            self.increase("__findchar.counter")
-        self.remove_variable("__findchar.counter")
+            self.increase("__indexchar.counter")
+        self.remove_variable("__indexchar.counter")
     
-    def findstr(self,value,str,return_variable):
-        self.initialize_variable("int","__findstr.counter")
-        self.assignment("__findstr.counter",0)
+    def indexstr(self,value,str,return_variable):
+        self.initialize_variable("int","__indexstr.counter")
+        self.assignment("__indexstr.counter",0)
         self.assignment(return_variable,-1)
-        self.initialize_variable("string","__findstr.sub")
+        self.initialize_variable("string","__indexstr.sub")
         for _ in range(len(value)-len(str)):
-            self.substr(value,self.V_VALUE[self.V_NAMES.find("__findstr.counter")],len(str),"__findstr.sub")
-            if self.V_VALUE[self.V_NAMES.find("__findstr.sub")]==str:
-                self.assignment(return_variable,self.V_VALUE[self.V_NAMES.find("__findstr.counter")])
-                self.remove_variable("__findstr.counter")
-                self.remove_variable("__findstr.sub")
+            self.substr(value,self.V_VALUE[self.V_NAMES.index("__indexstr.counter")],len(str),"__indexstr.sub")
+            if self.V_VALUE[self.V_NAMES.index("__indexstr.sub")]==str:
+                self.assignment(return_variable,self.V_VALUE[self.V_NAMES.index("__indexstr.counter")])
+                self.remove_variable("__indexstr.counter")
+                self.remove_variable("__indexstr.sub")
                 return
-            self.increase("__findstr.counter")
-        self.remove_variable("__findstr.counter")
-        self.remove_variable("__findstr.sub")
+            self.increase("__indexstr.counter")
+        self.remove_variable("__indexstr.counter")
+        self.remove_variable("__indexstr.sub")
 
     def is_string(self,value,return_variable):
         if value[0]=='"' and value[len(value)-1]=='"':
@@ -89,7 +90,7 @@ class compiler:
             self.initialize_variable("int","__is_int.counter")
             self.assignment("__is_int.counter",0)
             for _ in range(len(value)-1):
-                if value[self.V_VALUE[self.V_NAMES.find("__is_int.counter")]] not in "0123456789":
+                if value[self.V_VALUE[self.V_NAMES.index("__is_int.counter")]] not in "0123456789":
                     self.assignment(return_variable,0)
                     self.remove_variable("__is_int.counter")
                     return
@@ -104,7 +105,7 @@ class compiler:
             self.initialize_variable("int","__is_variable.counter")
             self.assignment("__is_variable.counter",0)
             for _ in range(len(value)-2):
-                if value[self.V_VALUE[self.V_NAMES.find("__is_variable.counter")]] not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.":
+                if value[self.V_VALUE[self.V_NAMES.index("__is_variable.counter")]] not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.":
                     self.assignment(return_variable,0)
                     self.remove_variable("__is_variable.counter")
                     return
@@ -125,7 +126,7 @@ class compiler:
         self.initialize_variable("int","__operation.counter")
         self.assignment("__operation.counter",0)
         for _ in range(len(self.OPERATION)):
-            if self.OPERATION[self.V_VALUE[self.V_NAMES.find("__operation.counter")]]==op:
+            if self.OPERATION[self.V_VALUE[self.V_NAMES.index("__operation.counter")]]==op:
                 if op=="=":
                     self.assignment(value1,value2)
                     self.remove_variable("__operation.counter")
@@ -156,23 +157,23 @@ class compiler:
     def evaluate(self,value,id,return_variable):
         self.initialize_variable("int","__evaluate.is_int")
         self.is_int(value,"__evaluate.is_int")
-        if self.V_VALUE[self.V_NAMES.find("__evaluate.is_int")]==1:
+        if self.V_VALUE[self.V_NAMES.index("__evaluate.is_int")]==1:
             self.assignment(return_variable,int(value))
             self.remove_variable("__evaluate.is_int")
             return
         self.remove_variable("__evaluate.is_int")
         self.initialize_variable("int","__evaluate.is_string")
         self.is_string(value,"__evaluate.is_string")
-        if self.V_VALUE[self.V_NAMES.find("__evaluate.is_string")]==1:
+        if self.V_VALUE[self.V_NAMES.index("__evaluate.is_string")]==1:
             self.substr(value,1,len(value)-2,return_variable)
             self.remove_variable("__evaluate.is_string")
             return
         self.remove_variable("__evaluate.is_string")
         self.initialize_variable("int","__evaluate.is_variable")
         self.is_variable(value,"__evaluate.is_variable")
-        if self.V_VALUE[self.V_NAMES.find("__evaluate.is_variable")]==1:
+        if self.V_VALUE[self.V_NAMES.index("__evaluate.is_variable")]==1:
             if value in self.V_NAMES:
-                self.assignment(return_variable,self.V_VALUE[self.V_NAMES.find(value)])
+                self.assignment(return_variable,self.V_VALUE[self.V_NAMES.index(value)])
                 self.remove_variable("__evaluate.is_variable")
                 return
             self.remove_variable("__evaluate.is_variable")
@@ -181,16 +182,16 @@ class compiler:
         self.initialize_variable("int","__evaluate.operation_counter"+str(id))
         self.assignment("__evaluate.operation_counter"+str(id),0)
         for _ in range(len(self.OPERATION)):
-            if self.OPERATION[self.V_VALUE[self.V_NAMES.find("__evaluate.operation_counter"+str(id))]] in value:
+            if self.OPERATION[self.V_VALUE[self.V_NAMES.index("__evaluate.operation_counter"+str(id))]] in value:
                 self.initialize_variable("string","__evaluate.value1"+str(id))
                 self.initialize_variable("string","__evaluate.value2"+str(id))
                 self.initialize_variable("int","__evaluate.op_index" +str(id))
-                self.findchar(value,self.OPERATION[self.V_VALUE[self.V_NAMES.find("__evaluate.operation_counter")]],"__evaluate.op_index"+str(id))
-                self.substr(value,0,self.V_VALUE[self.V_NAMES.find("__evaluate.op_index"+str(id))],"__evaluate.value1"+str(id))
-                self.substr(value,self.V_VALUE[self.V_NAMES.find("__evaluate.op_index"+str(id))]+1,len(value),"__evaluate.value2"+str(id))
-                self.evaluate(self.V_VALUE[self.V_NAMES.find("__evaluate.value1"+str(id))],id+1,"__evaluate.value1"+str(id))
-                self.evaluate(self.V_VALUE[self.V_NAMES.find("__evaluate.value2"+str(id))],id+1,"__evaluate.value2"+str(id))
-                self.operation(self.V_VALUE[self.V_NAMES.find("__evaluate.value1"+str(id))],self.V_VALUE[self.V_NAMES.find("__evaluate.value2"+str(id))],self.OPERATION[self.V_VALUE[self.V_NAMES.find("__evaluate.operation_counter"+str(id))]],return_variable)
+                self.indexchar(value,self.OPERATION[self.V_VALUE[self.V_NAMES.index("__evaluate.operation_counter")]],"__evaluate.op_index"+str(id))
+                self.substr(value,0,self.V_VALUE[self.V_NAMES.index("__evaluate.op_index"+str(id))],"__evaluate.value1"+str(id))
+                self.substr(value,self.V_VALUE[self.V_NAMES.index("__evaluate.op_index"+str(id))]+1,len(value),"__evaluate.value2"+str(id))
+                self.evaluate(self.V_VALUE[self.V_NAMES.index("__evaluate.value1"+str(id))],id+1,"__evaluate.value1"+str(id))
+                self.evaluate(self.V_VALUE[self.V_NAMES.index("__evaluate.value2"+str(id))],id+1,"__evaluate.value2"+str(id))
+                self.operation(self.V_VALUE[self.V_NAMES.index("__evaluate.value1"+str(id))],self.V_VALUE[self.V_NAMES.index("__evaluate.value2"+str(id))],self.OPERATION[self.V_VALUE[self.V_NAMES.index("__evaluate.operation_counter"+str(id))]],return_variable)
                 self.remove_variable("__evaluate.value1"+str(id))
                 self.remove_variable("__evaluate.value2"+str(id))
                 self.remove_variable("__evaluate.op_index"+str(id))
@@ -205,13 +206,13 @@ class compiler:
         self.assignment("__compile.types_counter",0)
         self.initialize_variable("string","__compile.code_sub")
         for _ in range(len(self.TYPES)):
-            self.substr(code,0,len(self.OPERATION(self.V_VALUE[self.V_NAMES.find("__compile.types_counter")]))+1,"__compile.code_sub")
-            if self.V_VALUE[self.V_NAMES.find("__compile.code_sub")]==self.OPERATION(self.V_VALUE[self.V_NAMES.find("__compile.types_counter")])+" ":
+            self.substr(code,0,len(self.OPERATION[self.V_VALUE[self.V_NAMES.index("__compile.types_counter")]])+1,"__compile.code_sub")
+            if self.V_VALUE[self.V_NAMES.index("__compile.code_sub")]==self.OPERATION(self.V_VALUE[self.V_NAMES.index("__compile.types_counter")])+" ":
                 self.initialize_variable("string","__compile.variable")
                 if "=" in code:
                     pass
-                self.substr(code,len(self.OPERATION(self.V_VALUE[self.V_NAMES.find("__compile.types_counter")]))+1,len(code),"__compile.variable")
-                self.initialize_variable(self.OPERATION(self.V_VALUE[self.V_NAMES.find("__compile.types_counter")]),self.V_VALUE[self.V_NAMES.find("__compile.variable")])
+                self.substr(code,len(self.OPERATION(self.V_VALUE[self.V_NAMES.index("__compile.types_counter")]))+1,len(code),"__compile.variable")
+                self.initialize_variable(self.OPERATION(self.V_VALUE[self.V_NAMES.index("__compile.types_counter")]),self.V_VALUE[self.V_NAMES.index("__compile.variable")])
                 self.remove_variable("__compile.variable")
                 self.remove_variable("__compile.code_sub")
                 self.remove_variable("__compile.types_counter")
@@ -225,6 +226,11 @@ class compiler:
         self.initialize_variable("int","__compile.counter")
         self.assignment("__compile.counter",0)
         for _ in range(len(CODE)):
-            self.compile_line(CODE[self.V_VALUE[self.V_NAMES.find("__compile.counter")]])
+            self.compile_line(CODE[self.V_VALUE[self.V_NAMES.index("__compile.counter")]])
             self.increase("__compile.counter")
         self.remove_variable("__compile.counter")
+
+c=compiler()
+c.compile()
+print(c.V_NAMES)
+print(c.V_VALUE)
